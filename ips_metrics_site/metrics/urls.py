018e210422da411a1b1f6c4d5187d0d6fc67d969
Benjamin.Diller <benjamin.diller@riotinto.com>
2019-10-16 12:45:34 -0600

    Preliminary (successful) 'upcoming' page

--- ips_metrics_site/metrics/urls.py
+++ ips_metrics_site/metrics/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('sandbox', views.sandbox, name='sandbox')
+    path('sandbox', views.sandbox, name='sandbox'),
+    path('upcoming', views.upcoming, name='upcoming'),
 ]
--- ips_metrics_site/metrics/views.py
+++ ips_metrics_site/metrics/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-
 # Create your views here.
 
 # One cool idea would be to have views that appear for individual IPF numbers, or tags. Something like the
@@ -9,62 +7,40 @@ from django.shortcuts import render
 
 # Might also be cool to try to build a pattern that'd return details of partial tags. So /metrics/tags/309
 # would return details for PT522309A, 309B, 525309A, etc. etc.
-from collections import namedtuple
+
 from datetime import datetime, timedelta
 
-from django.http import HttpResponse
 from django.apps import apps
+from django.http import HttpResponse
+from django.shortcuts import render
 
 Cell = apps.get_model("metrics", "Cell")
 
 
 def sandbox(request):
     """ Used for testing different ideas and behaviors """
-
-
     out_str = "Metrics Sandbox - for testing out ideas and such.<br>"
 
-    results = Cell.get_next_due(stop_date=datetime.now() + timedelta(days=280))
+    return HttpResponse(out_str)
+
+
+def upcoming(request):  # Will want to accept start_ and stop_date eventually, however that's done.
+    next_due = Cell.get_next_due(stop_date=datetime.now() + timedelta(days=90))  # All cells for IPFs coming due
 
     # Filter just the useful columns:
-    col_vals = ["Tag Number", "Description", "Plant", "Next Procedure Date"]
-    results = results.filter(col_header__value__in=col_vals).order_by('ipf_num')
-
-    # Define convenience object
-    # (May seriously want to think about creating something that's more universally useful)
-    # Can probably just use dict or something
-    Row = namedtuple('Row', 'ipf_num tag desc plant due_date')
+    col_headers = ["IPF Number", "Tag Number", "Description", "Plant", "Next Procedure Date"]
+    next_due = next_due.filter(col_header__value__in=col_headers[1:]).order_by('ipf_num')
 
     rows = list()
-    for ipf_num in set([result.ipf_num.value for result in results]):
-        row_cells = results.filter(ipf_num__value=ipf_num)
-        r = Row(ipf_num,
-                row_cells.filter(col_header__value="Tag Number")[0].date_or_content,
-                row_cells.filter(col_header__value="Description")[0].date_or_content,
-                row_cells.filter(col_header__value="Plant")[0].date_or_content,
-                row_cells.filter(col_header__value="Next Procedure Date")[0].date_or_content,
-                )
+    for ipf_num in set([cell.ipf_num.value for cell in next_due]):
+        row_cells = next_due.filter(ipf_num__value=ipf_num)
+        r = {"IPF Number": ipf_num}
+        r.update({col_header: row_cells.filter(col_header__value=col_header)[0].date_or_content
+                  for col_header in col_headers[1:]})
+
         rows.append(r)
 
-    # At this point I have two indexes - going to try to write some output with this data:
-    # Build table and header row:
-    out_str += "<table border='1'>"
-    out_str += "<tr>"
-    out_str += "<td>IPF Number</td>"
-    for col_val in col_vals:
-        out_str += f"<td>{col_val}</td>"
-    out_str += "</tr>"
-
-    # Build further table contents based on results of query:
-    for row in sorted(rows, key=lambda row: row.due_date):  # Allows for convenient sorting by header
-        out_str += "<tr>"
-        for i in range(len(Row._fields)):
-            val = row[i]
-            out_str += f"<td>{val}</td>"
-        out_str += "</tr>"
-    out_str += "</table>"
-
-    return HttpResponse(out_str)
+    return render(request, 'metrics/upcoming.html', {'rows': rows, 'col_headers': col_headers})
 
 # From: https://docs.djangoproject.com/en/2.2/intro/tutorial03/
 # Will want to use the following shortcuts for dealing with failures to retrieve DB items:
