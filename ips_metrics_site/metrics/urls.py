4150c88d4e0a79733dc8fcbefc00f9791555adb7
Benjamin.Diller <benjamin.diller@riotinto.com>
2020-02-18 15:11:29 -0700

    Clean up deprecated code and comments

--- ips_metrics_site/metrics/urls.py
+++ ips_metrics_site/metrics/urls.py
@@ -2,11 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.not_found, name='not_found'),
-    path('sandbox', views.sandbox, name='sandbox'),
+    path('', views.Upcoming.as_view(), name='upcoming'),
     path('upcoming/<int:days>', views.Upcoming.as_view(), name='upcoming'),
-    # path('ipf/<int:ipf_num>', views.IPFDetail.as_view(), name='ipf-detail'),
     path('ipf/<int:ipf_num>', views.IPFDetail.as_view(), name='ipf-detail'),
-    path('ipf-detail-loader', views.IPFDetailLoader.as_view(), name='ipf-detail-loader'),
     path('repopulate/<int:ipf_num>', views.Repopulate.as_view(), name='repopulate'),
 ]
--- ips_metrics_site/metrics/views.py
+++ ips_metrics_site/metrics/views.py
@@ -1,19 +1,8 @@
-# Create your views here.
-
-# One cool idea would be to have views that appear for individual IPF numbers, or tags. Something like the
-# polls detail views shown at https://docs.djangoproject.com/en/2.2/intro/tutorial03/
-
-# So, like /metrics/ipf/1/ would display the details for PT522309A
-
-# Might also be cool to try to build a pattern that'd return details of partial tags. So /metrics/tags/309
-# would return details for PT522309A, 309B, 525309A, etc. etc.
-
 from datetime import datetime, timedelta
 import json
 
 from django.apps import apps
-from django.core.exceptions import FieldError, ObjectDoesNotExist
-from django.db import IntegrityError
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
@@ -25,46 +14,12 @@ IPFNumber = apps.get_model("metrics", "IPFNumber")
 WorkSheet = apps.get_model("metrics", "WorkSheet")
 
 
-def not_found(request):
-    return HttpResponse('Nothing here yet.')
-
-
-def sandbox(request):
-    """ Used for testing different ideas and behaviors """
-    out_str = "Metrics Sandbox - for testing out ideas and such.<br>"
-
-    return HttpResponse(out_str)
-
-
-def load(ipf_num):
-    # Look for JSON file corresponding to this IPF num:
-    import os
-    fpath = os.path.join(r"C:\ProgProjects\ips-folder-crawler", f"{ipf_num}.json")
-    if os.path.isfile(fpath):
-        with open(fpath, 'r') as f:
-            content = json.load(f)
-        docs_blob = DocsBlob(ipf_num=ipf_num, content=content)
-        try:
-            docs_blob.save()
-        except IntegrityError:
-            return f"JSON contents for IPF #{ipf_num} already stored in DB!"
-        return f"Successfully loaded JSON for IPF #{ipf_num} into DB."
-
-    else:
-        return f"Could not locate JSON file for IPF #{ipf_num} at path: {fpath}"
-
-
 class Upcoming(View):
-    # TODO - Should I use Last Procedure Date somehow too? This whole thing breaks if "Next Procedure Date" is wrong
     col_headers = ["IPF Number", "Tag Number", "Type", "Description", "Plant", "Next Procedure Date", "Days Until Due"]
     sort_fields = {s.lower().replace(" ", ""): s for s in col_headers}  # Just a convenience object for sorting
     template = 'metrics/upcoming.html'
 
-    def get(self, request, days=30):  # Will want to accept start_ and stop_date eventually, however that's done.
-        # if sort_field is not "" and sort_field not in self.sort_fields:
-        #     raise FieldError(f"{sort_field} is not a valid field for sorting.")  # Can't sort by a non-existent field.
-            # I wonder if this should just 404 instead. # TODO Test this behavior
-
+    def get(self, request, days=30):
         stop_date = datetime.now() + timedelta(days=days)
         next_due = Cell.get_next_due(stop_date=stop_date)  # All cells for IPFs coming due
 
@@ -81,23 +36,10 @@ class Upcoming(View):
 
             rows.append(r)
 
-        # Sort rows (may be dynamic / user-configurable later):
-        # Default sorting that occurs before any user-specified sorting. May not want to keep this; not sure
+        # Default sorting that occurs before any user-specified sorting.
         rows = sorted(rows, key=lambda row: row["IPF Number"])
         rows = sorted(rows, key=lambda row: row["Days Until Due"])
 
-        # if sort_field:
-        #     rows = sorted(rows, key=lambda row: row[self.sort_fields[sort_field]], reverse=reverse)
-
-        # Another option for this might look like:
-        # orderbyList = ['check-in']  # default order
-        #
-        # if request.GET.getlist('order'):
-        #     orderbyList = request.GET.getlist('order')
-        #
-        # modelclassinstance.objects.all().order_by(*orderbyList)
-
-        # TODO Determine if self.col_headers needs to be returned anymore - probably not!
         return render(request, self.template, {'rows': rows, 'col_headers': self.col_headers, 'days': days})
 
 
@@ -113,7 +55,7 @@ class IPFDetail(View):
         try:
             # Necessary as try/except due to initial way data was stored
             docs_blob = json.loads(docs_blob.content)
-        except json.decoder.JSONDecodeError as e:
+        except json.decoder.JSONDecodeError:
             docs_blob = eval(docs_blob.content)
 
         content = dict()
@@ -146,45 +88,8 @@ class IPFDetail(View):
         return render(request, self.template, content)
 
 
-class IPFDetailLoader(View):
-    def get(self, request):
-        """Temporary View for loading all collected JSON data about IPF documents into DB."""
-        results = ""
-        latest_ws = WorkSheet.objects.latest('modified_time')
-        ipf_numbers = [i.value for i in IPFNumber.objects.all().filter(worksheet=latest_ws)]
-        for ipf_num in ipf_numbers:
-            results += f"<li>{ipf_num} | {load(ipf_num)}</li>"
-
-        return HttpResponse(f"<ul>{results}</ul>")
-
-
-class UpcomingDev(Upcoming):
-    """Simple approach to providing a dev-mode view that uses a different template"""
-    template = 'metrics/dev/upcoming.html'
-
-
-class IPFDetailDev(IPFDetail):
-    """Simple approach to providing a dev-mode view that uses a different template"""
-    template = 'metrics/dev/ipf-detail.html'
-
-
 class Repopulate(View):
     def post(self, response, ipf_num):
         doc_paths = DocsBlob.update(ipf_num)
         context = {'result': doc_paths, 'ipf_num': ipf_num}
         return render(response, 'metrics/ipf-repop.html', context)
-
-
-# From: https://docs.djangoproject.com/en/2.2/intro/tutorial03/
-# Will want to use the following shortcuts for dealing with failures to retrieve DB items:
-# from django.shortcuts import get_object_or_404, render
-# from .models import Cell
-# # ...
-# def detail(request, question_id):
-#     question = get_object_or_404(Cell, pk=question_id)
-#     return render(request, 'polls/detail.html', {'question': question})
-
-# There's also get_list_or_404() which uses filter() instead of get()
-
-# Check out generic.DetailView as shown here: https://docs.djangoproject.com/en/2.2/intro/tutorial04/
-# This would be useful for pages that link to an individual IPF number or similar
